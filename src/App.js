46439ea465dea4fa052fdbb66e01464a4bc5610e
Haoli Yang <hyang77@hawk.iit.edu>
2020-08-21 16:03:58 -0500

    update components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Particles from "react-particles-js";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -24,17 +26,37 @@ const app = new Clarifai.App({
   apiKey: "bb2bfac70b194b01b241c20028762e29",
 });
 
-
 class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
       input: "",
       imageURL: "",
+      box: {},
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.calcFaceLocation = this.calcFaceLocation.bind(this);
+    this.displayFaceBox = this.displayFaceBox.bind(this);
   }
+
+  calcFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      rightCol: width - clarifaiFace.right_col * width,
+      topRow: clarifaiFace.top_row * height,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
 
   handleInput = (evt) => {
     this.setState({ input: evt.target.value });
@@ -44,19 +66,16 @@ class App extends React.Component {
     console.log("submit!");
     this.setState({ imageURL: this.state.input });
     app.models
-  .predict(
-    Clarifai.FACE_DETECT_MODEL,
-    this.state.input
-  )
-  .then(
-    function (response) {
-      // do something with response
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function (err) {
-      // there was an error
-    }
-  );
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        // do something with response
+        console.log(response);
+        this.displayFaceBox(this.calcFaceLocation(response));
+      })
+      .catch((err) => {
+        // there was an error
+        console.log(err);
+      });
   };
 
   render() {
@@ -64,13 +83,15 @@ class App extends React.Component {
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
         <Navigation />
+        <Signin />
+        <Register />
         <Logo />
         <Rank />
         <ImageLinkForm
           handleInput={this.handleInput}
           handleSubmit={this.handleSubmit}
         />
-        <FaceRecognition imageURL={this.state.imageURL} />
+        <FaceRecognition box={this.state.box} imageURL={this.state.imageURL} />
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,9 +8,9 @@ const ImageLinkForm = ({handleInput, handleSubmit}) => {
         {"This Magic Brain will detect faces in your pictures. Git it a try."}
       </p>
       <div className="center">
-        <div className="input-background form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" onChange={handleInput}/>
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-gold" onClick={handleSubmit} >
+        <div className="form center pa3 br3 shadow-5">
+          <input className="f4 pa3 w-70 center" type="text" onChange={handleInput}/>
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-orange" onClick={handleSubmit} >
             Detect
           </button>
         </div>
