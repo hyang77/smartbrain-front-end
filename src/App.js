c986ad8f4eeed7af4ffc2be2ed4a422c11fafdb0
Haoli Yang <hyang77@hawk.iit.edu>
2020-08-20 23:07:02 -0500

    adding components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Particles from 'react-particles-js';
+import Particles from "react-particles-js";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Clarifai from "clarifai";
 
 const particlesOptions = {
   particles: {
@@ -12,24 +14,66 @@ const particlesOptions = {
       value: 100,
       density: {
         enable: true,
-        value_area: 800
-      }
-    }
-  }
-}
+        value_area: 800,
+      },
+    },
+  },
+};
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-          params={particlesOptions}
-        />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
+const app = new Clarifai.App({
+  apiKey: "bb2bfac70b194b01b241c20028762e29",
+});
+
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      input: "",
+      imageURL: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput = (evt) => {
+    this.setState({ input: evt.target.value });
+  };
+
+  handleSubmit = () => {
+    console.log("submit!");
+    this.setState({ imageURL: this.state.input });
+    app.models
+  .predict(
+    Clarifai.FACE_DETECT_MODEL,
+    this.state.input
+  )
+  .then(
+    function (response) {
+      // do something with response
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function (err) {
+      // there was an error
+    }
   );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          handleInput={this.handleInput}
+          handleSubmit={this.handleSubmit}
+        />
+        <FaceRecognition imageURL={this.state.imageURL} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({handleInput, handleSubmit}) => {
   return (
     <div>
       <p className="f3">
@@ -9,8 +9,8 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="input-background form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-gold">
+          <input className="f4 pa2 w-70 center" type="text" onChange={handleInput}/>
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-gold" onClick={handleSubmit} >
             Detect
           </button>
         </div>
